0c8abc0e365ba31f86afe9c85878156a130d809b
salmananwar <salmanzehri99@gmail.com>
2023-07-30 11:48:09 +0500

    v1-1-0

---
+++ src/main/java/api/test/service/ActivityEndPoints.java
@@ -0,0 +1,13 @@
+package api.test.service;
+
+public class ActivityEndPoints {
+
+    public static String base_url = "https://fakerestapi.azurewebsites.net";
+    public static String getActivitiesURL = base_url+"/api/v1/Activities";
+
+    public static String postActivityURL = base_url +"/api/v1/Activities";
+
+    public static String deleteActivityURL = base_url +"/api/v1/Activities";
+    public static String putActivityURL = base_url +"/api/v1/Activities";
+
+}
